cbe5c5d03e576ec756523c4e5cf768dc33e0ac92
srinu2301 <srinivas9743129622@gmail.com>
2021-04-08 16:28:03 +0530

    Add project files.

---
+++ APIDEMOCORE/Controllers/BikesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIDEMOCORE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BikesController : ControllerBase
+    {
+
+        List<string> bikes = initbikes();
+
+        public static List<string> initbikes()
+        {
+            var bikes = new List<string>() { "yamaha", "pulsar", "honda" };
+            return bikes;
+        }
+
+        // GET: api/<BikesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return bikes;
+        }
+
+        // GET api/<BikesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return bikes[id];
+        }
+
+        // POST api/<BikesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BikesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BikesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
